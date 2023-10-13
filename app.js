6b91b5dbfa292c04ecabbca6f4838f8f82d6abe9
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-10-14 00:21:41 +0900

    404 error page in express js

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
 
 app.use(adminRoute);
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express js</h1>");
+app.use(shopRoute);
+
+//404 error page
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found!</h1>");
 });
 
 app.listen(3000);

429fd7f1206e1daf80d39a665023920cf4118d20
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-10-14 01:14:51 +0900

    sending back error html file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const bodyParser = require("body-parser");
@@ -15,7 +16,7 @@ app.use(shopRoute);
 
 //404 error page
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found!</h1>");
+  res.status(404).sendFile(path.join(__dirname, "./", "views", "error.html"));
 });
 
 app.listen(3000);

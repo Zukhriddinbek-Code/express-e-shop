d2f1e304b7678bf2ecad48600a9656c70317d524
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-10-25 13:35:56 +0900

    creating a fs file to store products from user

--- models/product.js
+++ models/product.js
@@ -1,15 +1,20 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        products.push(this);
-    }
+  save() {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+  }
 
-    static fetchAll() {
-        return products;
-    }
-}
+  static fetchAll() {
+    return products;
+  }
+};

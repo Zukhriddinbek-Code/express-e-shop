7748e4533a66f6073b1f711dd533576265a74e1c
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-10-27 21:49:10 +0900

    done with mvc model

--- controllers/products.js
+++ controllers/products.js
@@ -17,7 +17,7 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll((prods) => {
+  Product.fetchAll((products) => {
     res.render("shop", {
       prods: products,
       pageTitle: "Shop",

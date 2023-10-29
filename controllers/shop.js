380c23571fed8e20e1bf029bbfe2706fae2cb972
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-10-29 21:33:35 +0900

    rendering a page for a specific product detail id

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,9 +13,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, (product) => {
-    console.log(product);
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: `'${product.title}' Details`,
+      path: "/products",
+    });
   });
-  res.redirect("/cart");
 };
 
 exports.getIndex = (req, res, next) => {

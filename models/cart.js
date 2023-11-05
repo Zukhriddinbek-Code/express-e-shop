7097395be795f40710391aae784acea5f40a5870
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-11-05 12:20:34 +0900

    bug not corrected cart.json

--- models/cart.js
+++ models/cart.js
@@ -33,4 +33,29 @@ module.exports = class Cart {
       });
     });
   };
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileCnt) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileCnt) };
+      const product = updatedCart.products.find((prod) => {
+        prod.id === id;
+      });
+      if (!product) {
+        return;
+      }
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        (prod) => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+        console.log(err);
+      });
+    });
+  }
 };

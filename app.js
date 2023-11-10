deedebcb8b29f81dcb2a2e5d5d1d1858a1520291
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-11-10 23:01:52 +0900

    syncing creating real tables in the sql database js definitions

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 
-const db = require("./util/database");
+const sequelize = require("./util/database");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -22,4 +22,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((res) => {
+    // console.log(res);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  }); //creates tables in sql database

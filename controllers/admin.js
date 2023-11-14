c70010fb24aa274d0bee9eebdaf69714b0a6d73b
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-11-14 23:55:33 +0900

    deleting product from database

--- controllers/admin.js
+++ controllers/admin.js
@@ -98,6 +98,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
+  // Product.deleteById(prodId);
+  Product.findByPk(prodId)
+    .then()
+    .catch((err) => {
+      console.log(err);
+    });
   res.redirect("/admin/products");
 };

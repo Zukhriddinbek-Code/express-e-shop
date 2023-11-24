e93f1fef26900f800681a9244db570549d73aaf6
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-11-25 03:14:02 +0900

    installing mongo db cloud service to our prod env

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
-const path = require("path");
-
+const app = express();
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const errorController = require("./controllers/error");
-
-const sequelize = require("./util/database");
-const Product = require("./models/product");
 const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
-
-const app = express();
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -27,14 +17,14 @@ app.use(express.static(path.join(__dirname, "public")));
 //creating a middleware for user
 //this will only register a middleware for incoming request
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 });
 
 app.use("/admin", adminRoutes);
@@ -42,38 +32,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // .sync({ force: true }) //this will overwrite the info with above relation of tables
-  .sync()
-  .then((res) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Max", email: "zuhriddin@mail.ru" });
-    }
-    return user;
-  })
-  .then((user) => {
-    // console.log(user);
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  }); //creates tables in sql database
-
-// // console.log(res);
-// app.listen(3000);
+app.listen(3000);
--- util/database.js
+++ util/database.js
@@ -1,14 +1,40 @@
-// const mysql = require("mysql2");
+//mongodb simple
+//installing packages
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "node-complete",
-//   password: "ganiev1017",
-// });
+//connecting to mongodb cloud
+const mongoConnect = (callback) => {
+  // cqc2jZjrxTHfH56e password for zuhriddin_ganiev user
+  MongoClient.connect(
+    "mongodb+srv://zuhriddin_ganiev:cqc2jZjrxTHfH56e@cluster-zuhriddin.65mbqpl.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("CONNECTED");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-// module.exports = pool.promise();
+module.exports = mongoConnect;
 
+/*
+//just sql
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: "ganiev1017",
+});
+module.exports = pool.promise();
+*/
+
+/*
+//sequelize
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("node-complete", "root", "ganiev1017", {
@@ -17,3 +43,4 @@ const sequelize = new Sequelize("node-complete", "root", "ganiev1017", {
 });
 
 module.exports = sequelize;
+*/

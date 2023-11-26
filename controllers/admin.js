51df7a528ce7928eb8ea1afc80b7f9d8213a0ee5
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-11-26 19:13:36 +0900

    finished the edit product page logic

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,7 @@
+const mongodb = require("mongodb");
 const Product = require("../models/product");
+
+const objectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -57,16 +60,17 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   //first find the product from sql data base
-  Product.findByPk(prodId)
-    .then((product) => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.imageUrl = updatedImageUrl;
-      product.description = updatedDesc;
-      //this will save data to the database, which is given by sequelize method
-      return product.save();
-    })
-    .then((res) => {
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    new objectId(prodId)
+  );
+  product
+    .save()
+    .then((result) => {
       console.log("UPDATED PRODUCT");
       res.redirect("/admin/products");
     })

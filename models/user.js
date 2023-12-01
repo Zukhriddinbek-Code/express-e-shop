10df771d591501e9deb234e1cf343899b409b02b
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-12-01 15:46:24 +0900

    string multiple prods in storage logic

--- models/user.js
+++ models/user.js
@@ -16,12 +16,24 @@ class User {
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex((cartProds) => {
-    //   return cartProds._id === product._id;
-    // });
+    const cartProductIndex = this.cart.items.findIndex((cartProds) => {
+      return cartProds.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new mongodb.ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
 
     const updatedCart = {
-      items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }],
+      items: updatedCartItems,
     };
     const db = getDb();
     return db

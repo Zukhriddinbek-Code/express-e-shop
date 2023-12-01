c642ef6f021f9ec8dcc1dfa036a59f71e4af0c9e
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-12-01 16:34:42 +0900

    displaying cart items

--- models/user.js
+++ models/user.js
@@ -44,6 +44,27 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((prod) => {
+          return {
+            ...prod,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === prod._id.toString();
+            }).quantity,
+          };
+        });
+      });
+  }
+
   //finding user
   static findById(userId) {
     const db = getDb();

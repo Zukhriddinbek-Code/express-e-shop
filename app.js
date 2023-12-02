106263decc46581f6805e710042f43812e6e7ae6
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-12-03 00:35:21 +0900

    installing mongoose and initializing

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const adminRoutes = require("./routes/admin");
@@ -34,6 +34,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://zuhriddin_ganiev:npdHaJuxSjeCV10K@cluster-zuhriddin.65mbqpl.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// npdHaJuxSjeCV10K

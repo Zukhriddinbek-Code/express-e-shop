ed7c808cccb2bc5443ded52115bb172644700025
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-12-12 00:09:12 +0900

    creating login.ejs file and adjusting css

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,6 @@
 exports.getLogin = (req, res, next) => {
-  Order.find({ "user.userId": req.user._id })
-    .then((orders) => {
-      res.render("shop/orders", {
-        path: "/orders",
-        pageTitle: "Your Orders",
-        orders: orders,
-      });
-    })
-    .catch((err) => console.log(err));
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+  });
 };

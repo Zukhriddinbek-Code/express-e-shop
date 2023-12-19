f69931f8ffce4a6019bd7d3b70d2f1edeb6fedc9
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-12-19 22:01:08 +0900

    new commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,29 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get("Cookie").trim().split("=")[1] === "true";
+  // console.log(isLoggedIn);
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: false,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  //setting a cookie
+  // max=age=10 (10secs and cookie will go away from client's browser)
+  //Domain='server'
+  //Secure //cookie will only be available in https not in http
+  //HttpOnly
+  // res.setHeader("Set-Cookie", "loggedIn=true"); //this will set a cookie with loggedIn variable to true
+
+  User.findById("65704bd15f9c3a9ccf14ced2")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };

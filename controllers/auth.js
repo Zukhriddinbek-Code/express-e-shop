e52ed79101d61f41eee34674af1e3e18693db4dc
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-12-19 23:49:39 +0900

    little bug when session.save() please redirect('/')

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,7 +23,11 @@ exports.postLogin = (req, res, next) => {
     .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      res.redirect("/");
+      //when session is saved then please redirect the page
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
     })
     .catch((err) => console.log(err));
 };

2df5de1efffbd2c2f982c538b7b25e5351bbc06b
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-01-05 23:00:06 +0900

    sending user given data to frontent to login page to validate inputs and show errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,6 +34,8 @@ exports.getLogin = (req, res, next) => {
     path: "/login",
     pageTitle: "Login",
     errorMessage: message,
+    oldInput: { email: "", password: "", confirmPassword: "" },
+    validationErrors: [],
   });
 };
 
@@ -72,6 +74,8 @@ exports.postLogin = (req, res, next) => {
       path: "/login",
       pageTitle: "Login",
       errorMessage: errors.array()[0].msg,
+      oldInput: { email: email, password: password },
+      validationErrors: errors.array(),
     });
   }
 

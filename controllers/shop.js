f76df53d4ccef272d2a97ccfb022254c16b2e87d
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-06 23:50:04 +0900

    adding dynamic page based on data received from backend

--- controllers/shop.js
+++ controllers/shop.js
@@ -59,7 +59,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: "Shop",
         path: "/",
-        totalProducts: totalItems,
+        currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,

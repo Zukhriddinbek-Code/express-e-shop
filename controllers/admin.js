ca6c6f5e0ac488e3b3d509ae34eceb6b264cc73a
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-07 00:50:43 +0900

    getting access to parentNode.querySelector() with vanilla js to get prodId and csrf

--- controllers/admin.js
+++ controllers/admin.js
@@ -197,28 +197,6 @@ exports.postEditProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   const page = +req.query.page || 1;
   let totalItems;
-
-  // Product.find()
-  //   .countDocuments()
-  //   .then((numProducts) => {
-  //     totalItems = numProducts;
-  //     return Product.find()
-  //       .skip((page - 1) * ITEMS_PER_PAGE)
-  //       .limit(ITEMS_PER_PAGE);
-  //   })
-  //   .then((products) => {
-  //     res.render("shop/product-list", {
-  //       prods: products,
-  //       pageTitle: "Products",
-  //       path: "/products",
-  //       currentPage: page,
-  //       hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-  //       hasPreviousPage: page > 1,
-  //       nextPage: page + 1,
-  //       previousPage: page - 1,
-  //       lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
-  //     });
-  //   })
   let message = req.flash("error");
   if (message.length > 0) {
     message = message[0];
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,4 @@
-const deleteProduct = () => {
-  console.log("clicked!");
+const deleteProduct = (btn) => {
+  const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
+  const prodId = btn.parentNode.querySelector("[name=productId]").value;
 };

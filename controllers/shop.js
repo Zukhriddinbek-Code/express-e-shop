759c17f540537890371c72f14c2179f1145cf014
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-03-07 16:47:23 +0900

    refactoring the backend route for /checkout and creating sessionId for stripe

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,9 @@ const PDFDocument = require("pdfkit");
 
 const fs = require("fs");
 const path = require("path");
+const stripe = require("stripe")(
+  "sk_test_51OrZlFDvrqqsjHbUIvstsaU1ZndxcK7hoiMX95TpO2spw5DU1f5ksHurOZK15JInb34zpxqNxKokFpxhj0nLrcUf00Y27K43gM"
+);
 
 const Product = require("../models/product");
 const Order = require("../models/order");
@@ -134,19 +137,40 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
   req.user
     .populate("cart.items.productId")
     .then((user) => {
-      const products = user.cart.items;
-      let total = 0;
+      products = user.cart.items;
+      total = 0;
       products.forEach((p) => {
         total += p.quantity * p.productId.price;
       });
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: products.map((p) => {
+          return {
+            name: p.productId.title,
+            description: p.productId.description,
+            amount: p.productId.price * 100,
+            currency: "usd",
+            quantity: p.quantity,
+          };
+        }),
+        success_url:
+          req.protocol + "://" + req.get("host") + "/checkout/success",
+        cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancel",
+      });
+    })
+    .then((session) => {
       res.render("shop/checkout", {
         path: "/checkout",
         pageTitle: "Checkout",
         products: products,
         totalSum: total,
+        sessionId: session.id,
       });
     })
     .catch((err) => {
